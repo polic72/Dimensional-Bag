f03203a086bb9fd9a8bc0359145eb26454af8e05
polic72 <>
2022-06-17 22:52:34 -0400

    Added Shift-Click functionality to the bags.

--- src/main/java/polic72/dimbag/container/BagContainer.java
+++ src/main/java/polic72/dimbag/container/BagContainer.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
@@ -34,10 +35,108 @@ public class BagContainer extends AbstractContainerMenu
 //		player = inventory.player;
 		playerInventory = new InvWrapper(inventory);
 		
-		
 		addSlotGrid(itemInventory, 0, 8, 18, 9, 18, 3, 18);
 		
 		layoutPlayerInventorySlots(8, 84);
+	}
+	
+	
+	@Override
+	public ItemStack quickMoveStack(Player player, int index)
+	{
+		ItemStack workingStack = ItemStack.EMPTY;
+		Slot slot = slots.get(index);
+		
+		if (slot != null && slot.hasItem())
+		{
+			ItemStack slotStack = slot.getItem();
+			workingStack = slotStack.copy();
+			
+			if (index < 27)
+			{
+				if (!this.moveItemStackTo(slotStack, 27, slots.size(), true))
+				{
+					return ItemStack.EMPTY;
+				}
+			}
+			else if (!this.moveItemStackTo(slotStack, 0, 27, false))
+			{
+				return ItemStack.EMPTY;
+			}
+			
+			if (slotStack.isEmpty())
+			{
+				slot.set(ItemStack.EMPTY);
+			}
+			else
+			{
+				slot.setChanged();
+			}
+		}
+		
+		return workingStack;
+		
+		
+//		ChestMenu
+//		
+////		super.quickMoveStack(player, index);
+//		
+////		Slot slot = slots.get(index);
+////		
+////		return super.quickMoveStack(player, index);
+//		ItemStack workingStack = ItemStack.EMPTY;
+//		Slot slot = this.slots.get(index);
+//		
+//		if (slot != null && slot.hasItem())
+//		{
+//			ItemStack selectedStack = slot.getItem();
+//			workingStack = selectedStack.copy();
+//			
+//			if (index == 0)
+//			{
+//				if (!this.moveItemStackTo(selectedStack, 1, 37, true))
+//				{
+//					return ItemStack.EMPTY;
+//				}
+////				slot.onQuickCraft(selectedStack, workingStack);
+//			}
+//			else
+//			{
+//				if (!this.moveItemStackTo(selectedStack, 0, 1, false))
+//				{
+//					return ItemStack.EMPTY;
+//				}
+//				else if (index < 28)
+//				{
+//					if (!this.moveItemStackTo(selectedStack, 28, 37, false))
+//					{
+//						return ItemStack.EMPTY;
+//					}
+//				}
+//				else if (index < 37 && !this.moveItemStackTo(selectedStack, 1, 28, false))
+//				{
+//					return ItemStack.EMPTY;
+//				}
+//			}
+//			
+//			if (selectedStack.isEmpty())
+//			{
+//				slot.set(ItemStack.EMPTY);
+//			}
+//			else
+//			{
+//				slot.setChanged();
+//			}
+//			
+//			if (selectedStack.getCount() == workingStack.getCount())
+//			{
+//				return ItemStack.EMPTY;
+//			}
+//			
+//			slot.onTake(playerIn, selectedStack);
+//		}
+//		
+//		return workingStack;
 	}
 	
 	
@@ -51,12 +150,13 @@ public class BagContainer extends AbstractContainerMenu
 	/**
 	 * Adds a row of slots.
 	 * 
-	 * @param handler The {@link IItemHandler} that the slots are getting their ItemStacks from.
-	 * @param index The index to start populating the slot row from.
-	 * @param x The X position of the first slot.
-	 * @param y The Y position of the first slot.
-	 * @param count The number of slots to create.
-	 * @param dx The space between the slots.
+	 * @param handler The {@link IItemHandler} that the slots are getting their
+	 *                ItemStacks from.
+	 * @param index   The index to start populating the slot row from.
+	 * @param x       The X position of the first slot.
+	 * @param y       The Y position of the first slot.
+	 * @param count   The number of slots to create.
+	 * @param dx      The space between the slots.
 	 * @return The index of the next slot immediately after this row.
 	 */
 	private int addSlotRow(IItemHandler handler, int index, int x, int y, int count, int dx)
@@ -76,14 +176,15 @@ public class BagContainer extends AbstractContainerMenu
 	/**
 	 * Adds a row of slots.
 	 * 
-	 * @param handler The {@link IItemHandler} that the slots are getting their ItemStacks from.
-	 * @param index The index to start populating the slot row from.
-	 * @param x The X position of the first slot.
-	 * @param y The Y position of the first slot.
-	 * @param xCount The number of slots to create horizontally.
-	 * @param dx The space between the slots horizontally.
-	 * @param yCount The number of slots to create vertically.
-	 * @param dy The space between the slots vertically.
+	 * @param handler The {@link IItemHandler} that the slots are getting their
+	 *                ItemStacks from.
+	 * @param index   The index to start populating the slot row from.
+	 * @param x       The X position of the first slot.
+	 * @param y       The Y position of the first slot.
+	 * @param xCount  The number of slots to create horizontally.
+	 * @param dx      The space between the slots horizontally.
+	 * @param yCount  The number of slots to create vertically.
+	 * @param dy      The space between the slots vertically.
 	 * @return The index of the next slot immediately after this grid.
 	 */
 	private int addSlotGrid(IItemHandler handler, int index, int x, int y, int xCount, int dx, int yCount, int dy)
@@ -102,14 +203,14 @@ public class BagContainer extends AbstractContainerMenu
 	 * Creates a player inventory as is standard in most GUIs that contain it.
 	 * 
 	 * @param left The position to start placing the inventory horizontally.
-	 * @param top The position to start placing the inventory vertically.
+	 * @param top  The position to start placing the inventory vertically.
 	 */
 	private void layoutPlayerInventorySlots(int left, int top)
 	{
-		//Player inventory:
+		// Player inventory:
 		addSlotGrid(playerInventory, 9, left, top, 9, 18, 3, 18);
 		
-		//Hotbar:
+		// Hotbar:
 		top += 58;
 		addSlotRow(playerInventory, 0, left, top, 9, 18);
 	}
